9f61e5bbe96aebcafda3d99a146c63dd911bcc48
mayk93 <mihai.mandrescu@gmail.com>
2016-04-10 17:27:06 +0100

    Working simple example.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@
  * Created by Michael on 08/04/16.
  */
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Basic Component.
 // Produces HTML.
 // Insert created HTML into DOM.
 
 const App = function () {
-    return <div>Hello!</div>
+    return <div>Hello!</div>;
 }
+
+ReactDOM.render(<App />, document.querySelector('.container'));
