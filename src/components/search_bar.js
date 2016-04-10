bf92f44135b54046c8cf7468ae4e1dcd2a4fb754
mayk93 <mihai.mandrescu@gmail.com>
2016-04-10 19:57:28 +0100

    React Tutorial - Refactor search as class.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,10 +2,25 @@
  * Created by Michael on 10/04/16.
  */
 
-import React from 'react'
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
-};
+class SearchBar extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {term: ''};
+    }
+
+    render () {
+        return <input onChange={ (event) => console.log(event.target.value) } />;
+    }
+
+    // Old handler - Replaced with inline handler.
+    /*
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+    */
+}
 
 export default SearchBar;
