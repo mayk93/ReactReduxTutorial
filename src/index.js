7f41c54ece189ecfd1df7706efd16fe3e0b66465
mayk93 <mihai.mandrescu@gmail.com>
2016-04-19 23:02:04 +0100

    Minor changes.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-import { YT_API } from './other/other_info.js';
+
+import API_KEYS from './other/other_info'; // Import entire dictionary
+//import { YT_API } from './other/other_info.js'; // Deconstructed version
 
 // Basic Component.
 // Produces HTML.
 // Insert created HTML into DOM.
 
+const YT_KEY = API_KEYS.YT_API;
+
+// <p>{ YT_KEY }</p>  // Used this to test corectness of API Key.
 const App = () => {
     return (
         <div>
+
             <SearchBar />
         </div>
     );
