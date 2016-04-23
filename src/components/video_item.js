1d887f60192350345ef63c01661d2995d87e4a35
mayk93 <mihai.mandrescu@gmail.com>
2016-04-23 01:19:13 +0100

    Not working correctly - Needs some love

--- src/components/video_item.js
+++ src/components/video_item.js
@@ -1,3 +1,11 @@
 /**
  * Created by Michael on 10/04/16.
  */
+
+import React from 'react';
+
+const VideoListItems = (props) => {
+    return <li>Video</li>
+};
+
+export default VideoListItems;
--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,10 +3,15 @@
  */
 
 import React from 'react';
+import VideoListItem from './video_list';
 
 const VideoList = (props) => {
+    // const videoItems = props.videos.map((video) => {
+    //    return <VideoListItem video={video} />
+    // });
     return (
       <ul className="col-md-4 list group">
+          // {videoItems}
           {props.videos.length}
       </ul>
     );
