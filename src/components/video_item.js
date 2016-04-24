10ddb8004ba696b3cbcb288fc2e4f3bf6da75ac1
mayk93 <mihai.mandrescu@gmail.com>
2016-04-24 01:59:38 +0100

    Getting thumbnails and titles.

--- src/components/video_item.js
+++ src/components/video_item.js
@@ -4,8 +4,32 @@
 
 import React from 'react';
 
+
+/*
+Instead of (props) and video = props.video, we can do ({video})
+This means we get the parameter props, and this props object has an attribute called video, and we use this in
+our scope.
+I won't use it, because I think it's confusing. It's clearer to use props.video. Explicit is better than implicit.
+*/
 const VideoListItem = (props) => {
-    return <li>Video</li>;
+    const video = props.video;
+    const imageURL = video.snippet.thumbnails.default.url;
+    const title = video.snippet.title;
+    return (
+      <li className="list-group-item">
+        <div className="video-list media">
+            <div className="media-left">
+                <img className="media-object" src={imageURL} />
+            </div>
+
+            <div className="media-body">
+                <div className="media-heading">
+                    {title}
+                </div>
+            </div>
+        </div>
+      </li>
+    );
 };
 
 export default VideoListItem;
